de817a6619c94a4195d3205033adb317614435c6
malandr1n0 <lindirizzoemail@gmail.com>
2022-03-10 10:29:23 +0100

    Formatted code

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,63 @@
-let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-    name: 'Bulbasaur',
-    height: 70, //Expressed in cm
-    weight: 6.9, //Expressed in kg
-    catchRate: 0, //Expressed in %
-    evolvingLevel: 16,
-    types: ['grass', 'poison']
-    },
-    { 
-    name: 'Charmander',
-    height: 60, //Expressed in cm
-    weight: 8.5, //Expressed in kg
-    catchRate: 0, //Expressed in %
-    evolvingLevel: 16,
-    types: ['fire']
-    },
-    { 
-    name: 'Squirtle',
-    height: 50, //Expressed in cm
-    weight: 9, //Expressed in kg
-    catchRate: 0, //Expressed in %
-    evolvingLevel: 16,
-    types: ['water']
-    },
-    { 
-    name: 'Nidoran',
-    height: 50, //Expressed in cm
-    weight: 9, //Expressed in kg
-    catchRate: 0, //Expressed in %
-    evolvingLevel: 16,
-    types: ['poison']
-    },
-    { 
-    name: 'Magikarp',
-    height: 90, //Expressed in cm
-    weight: 10, //Expressed in kg
-    catchRate: 0, //Expressed in %
-    evolvingLevel: 20,
-    types: ['water']
-    } 
-];
+let pokemonRepository = (function() {
+    let pokemonList = [{
+            name: 'Bulbasaur',
+            height: 70, //Expressed in cm
+            weight: 6.9, //Expressed in kg
+            catchRate: 0, //Expressed in %
+            evolvingLevel: 16,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 60, //Expressed in cm
+            weight: 8.5, //Expressed in kg
+            catchRate: 0, //Expressed in %
+            evolvingLevel: 16,
+            types: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 50, //Expressed in cm
+            weight: 9, //Expressed in kg
+            catchRate: 0, //Expressed in %
+            evolvingLevel: 16,
+            types: ['water']
+        },
+        {
+            name: 'Nidoran',
+            height: 50, //Expressed in cm
+            weight: 9, //Expressed in kg
+            catchRate: 0, //Expressed in %
+            evolvingLevel: 16,
+            types: ['poison']
+        },
+        {
+            name: 'Magikarp',
+            height: 90, //Expressed in cm
+            weight: 10, //Expressed in kg
+            catchRate: 0, //Expressed in %
+            evolvingLevel: 20,
+            types: ['water']
+        }
+    ];
 
-  return {
-    add: function(pokemon) {
-      //Checking if the pokemon added is an Object
-      if (typeof pokemon === 'object') pokemonList.push(pokemon);
-      else alert("Not a valid pokemon");
-    },
-    getAll: function() {
-      return pokemonList;
-    },
-    //Find a pokemon by name
-    find: function(text){
-      const result = pokemonList.filter(singlePokemon => singlePokemon.name === text);
-      console.log(result);
-      if (result.length === 0) alert("Pokemon Not Found");
-      else alert("Pokemon found");
-    }
-  };
+    return {
+        add: function(pokemon) {
+            //Checking if the pokemon added is an Object
+            if (typeof pokemon === 'object') pokemonList.push(pokemon);
+            else alert("Not a valid pokemon");
+        },
+        getAll: function() {
+            return pokemonList;
+        },
+        //Find a pokemon by name
+        find: function(text) {
+            const result = pokemonList.filter(singlePokemon => singlePokemon.name === text);
+            console.log(result);
+            if (result.length === 0) alert("Pokemon Not Found");
+            else alert("Pokemon found");
+        }
+    };
 })();
 
 
@@ -73,33 +72,33 @@ pokemonList.forEach(createPokemonCard);
 function createPokemonCard(singlePokemon) {
     //Create a div with pokemon__item class
     const pokemonItemDiv = document.createElement('div');
-    
+
     //Define class name for new div
     pokemonItemDiv.className = 'pokemon__item';
     container.appendChild(pokemonItemDiv);
 
     //Insert text into new div
-    pokemonItemDiv.innerHTML ='Name:        '+singlePokemon.name+'<br>'+
-                              'Height:      '+singlePokemon.height+'<br>'+
-                              'Weight:      '+singlePokemon.weight+'<br>'+
-                              'Catch Rate:  '+singlePokemon.catchRate+'<br>'+
-                              'Evolving at: '+singlePokemon.evolvingLevel;  
-    
+    pokemonItemDiv.innerHTML = 'Name:        ' + singlePokemon.name + '<br>' +
+        'Height:      ' + singlePokemon.height + '<br>' +
+        'Weight:      ' + singlePokemon.weight + '<br>' +
+        'Catch Rate:  ' + singlePokemon.catchRate + '<br>' +
+        'Evolving at: ' + singlePokemon.evolvingLevel;
+
     //Check Height of Pokemon                              
-        if (singlePokemon.height > height) pokemonItemDiv.classList.add('big__pokemon');
+    if (singlePokemon.height > height) pokemonItemDiv.classList.add('big__pokemon');
 }
 
 //Test for find() function
 //pokemonRepository.find('Bulbasaure');
 
 //Test for pokemonRepository.add
-pokemonRepository.add({name: 'Magikarp',
+pokemonRepository.add({
+    name: 'Magikarp',
     height: 90, //Expressed in cm
     weight: 10, //Expressed in kg
     catchRate: 0, //Expressed in %
     evolvingLevel: 20,
-    types: ['water']});
+    types: ['water']
+});
 
 console.table(pokemonList);
-
-
