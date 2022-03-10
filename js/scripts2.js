cf2e6cfbbddf6e3b558a11fc64d2065642fbb394
malandr1n0 <lindirizzoemail@gmail.com>
2022-03-10 10:06:14 +0100

    Check type of value for add function, implemented function find

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,4 +1,5 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
     name: 'Bulbasaur',
     height: 70, //Expressed in cm
@@ -41,10 +42,32 @@ let pokemonList = [
     } 
 ];
 
+  return {
+    add: function(pokemon) {
+      //Checking if the pokemon added is an Object
+      if (typeof pokemon === 'object') pokemonList.push(pokemon);
+      else alert("Not a valid pokemon");
+    },
+    getAll: function() {
+      return pokemonList;
+    },
+    //Find a pokemon by name
+    find: function(text){
+      const result = pokemonList.filter(singlePokemon => singlePokemon.name === text);
+      console.log(result);
+      if (result.length === 0) alert("Pokemon Not Found");
+      else alert("Pokemon found");
+    }
+  };
+})();
+
+
 const container = document.querySelector(".container");
 const height = 69;
 
 //Iterate over pokemonList taking a function as parameter (Object, index, array)
+let pokemonList = pokemonRepository.getAll();
+
 pokemonList.forEach(createPokemonCard);
 
 function createPokemonCard(singlePokemon) {
@@ -54,7 +77,7 @@ function createPokemonCard(singlePokemon) {
     //Define class name for new div
     pokemonItemDiv.className = 'pokemon__item';
     container.appendChild(pokemonItemDiv);
-    
+
     //Insert text into new div
     pokemonItemDiv.innerHTML ='Name:        '+singlePokemon.name+'<br>'+
                               'Height:      '+singlePokemon.height+'<br>'+
@@ -63,10 +86,20 @@ function createPokemonCard(singlePokemon) {
                               'Evolving at: '+singlePokemon.evolvingLevel;  
     
     //Check Height of Pokemon                              
-        if (singlePokemon.height > height) {
-            pokemonItemDiv.classList.add('big__pokemon');
-        }
+        if (singlePokemon.height > height) pokemonItemDiv.classList.add('big__pokemon');
 }
 
+//Test for find() function
+//pokemonRepository.find('Bulbasaure');
 
-   
+//Test for pokemonRepository.add
+pokemonRepository.add({name: 'Magikarp',
+    height: 90, //Expressed in cm
+    weight: 10, //Expressed in kg
+    catchRate: 0, //Expressed in %
+    evolvingLevel: 20,
+    types: ['water']});
+
+console.table(pokemonList);
+
+
